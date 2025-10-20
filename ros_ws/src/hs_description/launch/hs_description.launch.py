ddd713e4e60d49c0d0c1cbca146442ee29f1dcd9
Vladislav Saburov <vsv24001@student.mdu.se>
2025-10-20 13:31:50 +0200

    convert urdf files to sdf

--- ros_ws/src/hs_description/launch/hs_description.launch.py
+++ ros_ws/src/hs_description/launch/hs_description.launch.py
@@ -27,7 +27,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     description_dir = get_package_share_directory('hs_description')
     xacro_file = PathJoinSubstitution(
-        [description_dir, 'urdf', 'hs.urdf.xacro'])
+        [description_dir, 'sdf', 'hs.sdf.xacro'])
 
     ARGUMENTS = [DeclareLaunchArgument(
         'namespace', default_value='hs', description='Robot namespace')]
