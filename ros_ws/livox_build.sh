faa8d7b71d71ee1442329bcb0c10b5d14fce0859
Andreas <apn21017@student.mdu.se>
2025-10-22 13:53:52 +0200

    Moved around some files and folders

--- ros_ws/livox_build.sh
+++ ros_ws/livox_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Downloand and build/install Livox-SDK2
-cd /slarc_ws/src/Livox-SDK2/
+cd /third_party_sdk_drivers/Livox-SDK2/
 cat sdk_core/logger_handler/file_manager.h | grep '#include <cstdint>' || sed -i '31i #include <cstdint>' sdk_core/logger_handler/file_manager.h
 cat sdk_core/comm/define.h | grep '#include <cstdint>' || sed -i '34i #include <cstdint>' sdk_core/comm/define.h
 mkdir build
@@ -14,3 +14,10 @@ cd /slarc_ws/src/livox_ros_driver2
 source /opt/ros/jazzy/setup.bash
 ./build.sh humble
 cd /
+
+# Configure dynamic linker run-time bindings
+echo /usr/local/lib | sudo tee /etc/ld.so.conf.d/livox-sdk2.conf
+sudo ldconfig
+
+# Source
+source /slarc_ws/install/setup.bash
