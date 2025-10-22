0be7723213e10ab224be0753dc7954d5feaecb2e
Sebastian Fryklund <sfd21003@student.mdu.se>
2025-10-22 13:43:16 +0200

    adapted health_chack node to new topic for allowed modes

--- ros_ws/src/hs_robot/src/health_check.cpp
+++ ros_ws/src/hs_robot/src/health_check.cpp
@@ -1,85 +1,93 @@
 
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/bool.hpp"
+#include <chrono>
 
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/bool.hpp>
+#include "hs_msgs/msg/operation_modes.hpp"
 
 
 class HealthCheckNode : public rclcpp::Node {
 
     public:
     HealthCheckNode()
-     : Node("health_check"), h_emergency_stop(true){
+     : Node("health_check"), h_emergency_stop(true) {
 
-        using std::placeholders::_1;
-
-        allow_auto.data = false;
-        allow_telop.data = false;
-
-        
         rclcpp::QoS qos_profile(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_sensor_data));
         qos_profile.reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
 
-        sub_emergency_stop = this->create_subscription<std_msgs::msg::Bool>("emergency_stop", qos_profile, std::bind(&HealthCheckNode::callback_emergency_stop, this, _1));
+        // set initial allowed modes before sensor data can be read
+        allowed_modes.local_telop = false;
+        allowed_modes.remote_telop = false;
+        allowed_modes.autonomy_drive = false;
 
-        pub_health_allow_telop = this->create_publisher<std_msgs::msg::Bool>("health_allow_telop", 5);
-        pub_health_allow_auto = this->create_publisher<std_msgs::msg::Bool>("health_allow_auto", 5);
+        sub_emergency_stop = this->create_subscription<std_msgs::msg::Bool>("emergency_stop", qos_profile, std::bind(&HealthCheckNode::callback_emergency_stop, this, std::placeholders::_1));
+
+        pub_allowed_operation_modes = this->create_publisher<hs_msgs::msg::OperationModes>("allowed_operation_modes", 10);
+
+        using namespace std::chrono_literals;
+        timer_health_update = this->create_timer(500ms, std::bind(&HealthCheckNode::callback_timer_health_update, this));
+
+        RCLCPP_INFO(this->get_logger(), "Health Checks Is Running");
+    }
+
+    void callback_timer_health_update() {
+
+        RCLCPP_INFO(this->get_logger(), "Timer Callback");
+        
+        publishAllowedModes();
+        return;
     }
 
     void callback_emergency_stop(std_msgs::msg::Bool emergency_stop) {
 
-        RCLCPP_INFO(this->get_logger(), "Stop: %d", (int) emergency_stop.data);
+        // do nothing as nothing has changed
+        if (h_emergency_stop == emergency_stop.data)
+            return;
 
         h_emergency_stop = emergency_stop.data;
 
-        checkHealth();
+        if(h_emergency_stop) {
+            allowed_modes.local_telop = false;
+            allowed_modes.remote_telop = false;
+            allowed_modes.autonomy_drive = false;
+        }
+        else checkHealth();
+        
+        timer_health_update.reset();
         publishAllowedModes();
     }
 
     private:
 
-    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr pub_health_allow_telop;
-    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr pub_health_allow_auto;
+    rclcpp::Publisher<hs_msgs::msg::OperationModes>::SharedPtr pub_allowed_operation_modes;
+    rclcpp::TimerBase::SharedPtr timer_health_update;
 
     rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr sub_emergency_stop;
 
     bool h_emergency_stop;
-
-    std_msgs::msg::Bool allow_telop;
-    std_msgs::msg::Bool allow_auto;
-
+    hs_msgs::msg::OperationModes allowed_modes;
     
 
-    // function indicates allowed operation modes based on critical parameters
+    // function indicates allowed operation modes based on non critical parameters
     void checkHealth() {
 
-        // maybe turn this around to start as false
-        allow_telop.data = true;
-        allow_auto.data = true;
+        allowed_modes.local_telop = true;
+        allowed_modes.remote_telop = true;
+        allowed_modes.autonomy_drive = true;
 
-        if(h_emergency_stop) {
-            allow_telop.data = false;
-            allow_auto.data = false;
-            return;
-        }
-    
         return;
     }
 
-    void publishAllowedModes() {
-        this->pub_health_allow_telop->publish(allow_telop);
-        this->pub_health_allow_auto->publish(allow_auto); 
+    inline void publishAllowedModes() {
+        this->pub_allowed_operation_modes->publish(allowed_modes);
     }
 
-
 };
-
-
-
 
 int main (int argv, char* argc[]) {
 
     rclcpp::init(argv, argc);
     rclcpp::spin(std::make_shared<HealthCheckNode>());
     rclcpp::shutdown();
-    
+
 }
