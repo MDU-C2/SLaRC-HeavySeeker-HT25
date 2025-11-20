4cf81639ac66fd868083cd227d6987f5daaeae36
Vladislav Saburov <vsv24001@student.mdu.se>
2025-11-20 13:00:13 +0100

    test description on husky

---
+++ ros_ws/src/hs_robot/launch/test.launch.py
@@ -0,0 +1,118 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node, SetRemap
+from launch.actions import IncludeLaunchDescription, GroupAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+
+################### configs start ###################
+autostart = 'true'
+use_lifecycle_manager = 'false'
+use_sim_time = 'false'
+slam_params_file = PathJoinSubstitution([get_package_share_directory(
+    "hs_robot"), "conf", "slam_async_config.yaml"])
+nav2_params_file = PathJoinSubstitution(
+    [get_package_share_directory("hs_robot"), "conf", "nav2_params.yaml"])
+output_velocity_topic = "platform/cmd_vel"
+################### configs end #####################
+
+
+def generate_launch_description():
+    # Directories
+    hs_bringup_dir = get_package_share_directory('hs_bringup')
+    slam_toolbox_dir = get_package_share_directory('slam_toolbox')
+    nav2_bringup_dir = get_package_share_directory('nav2_bringup')
+    hs_description_dir = get_package_share_directory('hs_description')
+    hs_robot_dir = get_package_share_directory('hs_robot')
+
+    ekf_conf = PathJoinSubstitution(
+        [hs_robot_dir, 'conf', 'localization.yaml'])
+
+    # Descriptions
+
+    # Robot_State_Publisher
+    description_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource(
+        PathJoinSubstitution([hs_description_dir, 'launch', 'hs_description.launch.py'])))
+
+    desc_group = GroupAction([SetRemap(
+        src='joint_states', dst='/a200_0309/platform/joint_states'), description_launch])
+
+    # EKF
+    ekf_node = Node(
+        package="robot_localization",
+        executable="ekf_node",
+        name="ekf_test_node",
+        output="screen",
+        parameters=[ekf_conf],
+    )
+
+    # Lidar
+    livox_lidar_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([hs_bringup_dir, 'launch', 'livox_launch.py'])
+        ),
+    )
+
+    # Cloud2Scan
+    cloud2scan_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([hs_bringup_dir, 'launch',
+                                 'cloud2scan.launch.py'])
+        ),
+    )
+
+    # SLAM
+    slam_toolbox_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([slam_toolbox_dir, 'launch',
+                                 'online_sync_launch.py'])
+        ),
+        launch_arguments={
+            'autostart': autostart,
+            'use_lifecycle_manager': use_lifecycle_manager,
+            'use_sim_time': use_sim_time,
+            'slam_params_file': slam_params_file,
+        }.items(),
+    )
+
+    # NAV2
+    nav2_bringup_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [nav2_bringup_dir, 'launch', 'bringup_launch.py'])
+        ),
+        launch_arguments={
+            'autostart': autostart,
+            'use_sim_time': use_sim_time,
+            'params_file': nav2_params_file,
+        }.items(),
+    )
+
+    nav2_group = GroupAction([SetRemap(
+        src='platform/cmd_vel', dst='/a200_0309/platform/cmd_vel'), nav2_bringup_launch])
+
+    # Group
+    group = GroupAction([
+        desc_group,
+        ekf_node,
+        livox_lidar_launch,
+        cloud2scan_launch,
+        slam_toolbox_launch,
+        nav2_group,
+    ])
+
+    # Frame
+    # livox_frame = Node(
+    #     package='tf2_ros',
+    #     executable='static_transform_publisher',
+    #     name='livox_frame_static_broadcaster',
+    #     output='screen',
+    #     arguments=['0.17', '0.0', '0.18', '3.1415926', '0.0',
+    #                '0.0', 'top_plate_rear_mount', 'livox_frame'],
+    # )
+
+    return LaunchDescription([
+        group,
+        # livox_frame,
+    ])
