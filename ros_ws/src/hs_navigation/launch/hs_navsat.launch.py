c9cd2c64c10064717f8ef24745aa97a5856e29d0
Vladislav Saburov <vsv24001@student.mdu.se>
2025-12-02 09:17:19 +0100

    ekf launch fix

--- ros_ws/src/hs_navigation/launch/hs_navsat.launch.py
+++ ros_ws/src/hs_navigation/launch/hs_navsat.launch.py
@@ -75,7 +75,7 @@ def generate_launch_description():
         parameters=[dual_ekf_navsat_config, {"use_sim_time": use_sim_time}],
         remappings=[
             ("oakd/imu", "imu/data"),
-            ("navsatfix", "gps/fix"),
+            ("gps/fix", "navsatfix"),
             ("gps/filtered", "gps/filtered"),
             ("odometry/gps", "odometry/gps"),
             ("odometry/filtered", "odometry/global"),
