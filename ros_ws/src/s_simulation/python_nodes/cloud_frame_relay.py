d83c66f041682105b117f69e10a76fc1d4279de6
Mattias Tidström <mtm20001@student.mdu.se>
2025-12-10 16:14:13 +0100

    fixed the sensor rig to match the current configuration

--- ros_ws/src/s_simulation/python_nodes/cloud_frame_relay.py
+++ ros_ws/src/s_simulation/python_nodes/cloud_frame_relay.py
@@ -8,9 +8,13 @@ IN  = '/lidar_points'       # or the name you bridged to
 OUT = '/lidar_points_fixed'
 FRAME = 'livox_frame'
 
-IN_2  = '/oakd_points'
-OUT_2 = '/oakd_points_fixed'
-FRAME_2 = 'oakd_pro_rgb_camera_optical_frame'
+IN_2  = '/oakd_pro_1/oakd/rgbd/points'
+OUT_2 = '/oakd_points_1_fixed'
+FRAME_2 = 'oakd_pro_1_rgb_camera_frame'
+
+IN_3  = '/oakd_pro_2/oakd/rgbd/points'
+OUT_3 = '/oakd_points_2_fixed'
+FRAME_3 = 'oakd_pro_2_rgb_camera_frame'
 
 class Relay(Node):
     def __init__(self):
@@ -18,16 +22,23 @@ class Relay(Node):
         self.pub_lidar = self.create_publisher(PointCloud2, OUT, 10)
         self.sub_lidar = self.create_subscription(PointCloud2, IN, self.cb_lidar, 10)
 
-        self.pub_oakd = self.create_publisher(PointCloud2, OUT_2, 10)
-        self.sub_oakd = self.create_subscription(PointCloud2, IN_2, self.cb_oakd, 10)
+        self.pub_oakd_1 = self.create_publisher(PointCloud2, OUT_2, 10)
+        self.sub_oakd_1= self.create_subscription(PointCloud2, IN_2, self.cb_oakd1, 10)
+
+        self.pub_oakd_2 = self.create_publisher(PointCloud2, OUT_3, 10)
+        self.sub_oakd_2= self.create_subscription(PointCloud2, IN_3, self.cb_oakd2, 10)
 
     def cb_lidar(self, msg):
         msg.header.frame_id = FRAME
         self.pub_lidar.publish(msg)
 
-    def cb_oakd(self, msg):
+    def cb_oakd1(self, msg):
         msg.header.frame_id = FRAME_2
-        self.pub_oakd.publish(msg)
+        self.pub_oakd_1.publish(msg)
+
+    def cb_oakd2(self, msg):
+        msg.header.frame_id = FRAME_3
+        self.pub_oakd_2.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
