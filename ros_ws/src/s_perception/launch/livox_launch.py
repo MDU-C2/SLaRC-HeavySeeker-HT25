3e0e4751f1b97b0d716f282937bccdbb58db45e4
Rasmus <rln20004@student.mdu.se>
2025-12-16 10:29:03 +0100

    Test was successful

--- ros_ws/src/s_perception/launch/livox_launch.py
+++ ros_ws/src/s_perception/launch/livox_launch.py
@@ -1,4 +1,5 @@
 import os
+import json
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
@@ -6,30 +7,28 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 
 ################### user configure parameters for ros2 start ###################
-xfer_format   = 0    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format
-multi_topic   = 1    # 0-All LiDARs share the same topic, 1-One LiDAR one topic
-data_src      = 0    # 0-lidar, others-Invalid data src
-publish_freq  = 10.0 # freqency of publish, 5.0, 10.0, 20.0, 50.0, etc.
-output_type   = 0
-frame_id      = 'livox_frame'
-lvx_file_path = '/home/livox/livox_test.lvx'
-cmdline_bd_code = 'livox0000000001'
+
 
 cur_path = os.path.split(os.path.realpath(__file__))[0] + '/'
 cur_config_path = cur_path + '../config'
 user_config_path = os.path.join(cur_config_path, 'MID360_config.json')
+
+
+with open(os.path.join(cur_config_path, 'param_config.json')) as f:
+    param_config = json.load(f)
+
 ################### user configure parameters for ros2 end #####################
 
 livox_ros2_default_params = {
-    "xfer_format": xfer_format,
-    "multi_topic": multi_topic,
-    "data_src": data_src,
-    "publish_freq": publish_freq,
-    "output_data_type": output_type,
-    "frame_id": frame_id,
-    "lvx_file_path": lvx_file_path,
+    "xfer_format": param_config.get("xfer_format", 0),
+    "multi_topic": param_config.get("multi_topic", 1),
+    "data_src": param_config.get("data_src", 0),
+    "publish_freq": param_config.get("publish_freq", 10.0),
+    "output_data_type": param_config.get("output_type", 0),
+    "frame_id": param_config.get("frame_id", "livox_frame"),
+    "lvx_file_path": param_config.get("lvx_file_path", "/home/livox/livox_test.lvx"),
     "user_config_path": user_config_path,
-    "cmdline_input_bd_code": cmdline_bd_code
+    "cmdline_input_bd_code": param_config.get("cmdline_bd_code", "livox0000000001")
 }
 
 
