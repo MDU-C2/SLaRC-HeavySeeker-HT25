514d84ab5e7a2b90e205d188c36a9d32d27d3c0d
Vladislav Saburov <vsv24001@student.mdu.se>
2025-12-17 15:31:24 +0100

    oakd imu orientation

--- ros_ws/src/hs_navigation/hs_navigation/oakd_rot_vector.py
+++ ros_ws/src/hs_navigation/hs_navigation/oakd_rot_vector.py
@@ -9,23 +9,49 @@ from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Quaternion
 import threading
 import tf_transformations as tf
+import numpy as np
 
 
 class OakdRotVector(Node):
     def __init__(self):
         super().__init__('oakd_rotation_vec')
         self.imu_pub = self.create_publisher(Imu, 'oakd/imu/rotation', 10)
+        self.pure_imu_pub = self.create_publisher(Imu, 'oakd/imu/data', 10)
 
     def publish_quaternion(self, x, y, z, w):
-        msg = Imu()
-        orientation = Quaternion()
-        (roll, pitch, yaw) = tf.euler_from_quaternion((x, y, z, w))
-        yaw = -yaw + (math.pi / 2) 
-        (orientation.x, orientation.y, orientation.z, orientation.w) = tf.quaternion_from_euler(roll, pitch, yaw)
-        msg.orientation = orientation
-        msg.header.frame_id = 'oakd_imu_frame'
-        msg.header.stamp = self.get_clock().now().to_msg()
-        self.imu_pub.publish(msg)
+        msg1 = Imu()
+        msg2 = Imu()
+        orientation1 = Quaternion()
+        orientation2 = Quaternion()
+        frame = 'oakd_imu_frame'
+
+        R = tf.quaternion_matrix(tf.quaternion_inverse((x, y, z, w)))[:3, :3]
+        R_wsd2enu = np.array([
+            [-1, 0, 0],
+            [0, -1, 0],
+            [0, 0, -1]
+        ])
+        R_enu = R_wsd2enu @ R
+        q_old = tf.quaternion_from_matrix(np.pad(R_enu, ((0, 1), (0, 1)), 'constant', constant_values=0))
+
+        (orientation1.x, orientation1.y, orientation1.z,
+         orientation1.w) = tf.quaternion_multiply(q_old, tf.quaternion_about_axis(math.pi / 2, [0, 0, 1]))
+        
+        (orientation2.x, orientation2.y,
+         orientation2.z, orientation2.w) = q_old
+        
+        msg1.orientation = orientation1
+        msg1.orientation_covariance[0] = -1.0
+        msg1.orientation_covariance[4] = -1.0
+        msg1.orientation_covariance[8] = -1.0
+        msg1.header.frame_id = frame
+        msg2.orientation = orientation2
+        msg2.header.frame_id = frame
+        t = self.get_clock().now().to_msg()
+        msg1.header.stamp = t
+        msg2.header.stamp = t
+        self.imu_pub.publish(msg1)
+        self.pure_imu_pub.publish(msg2)
 
 
 def main(args=None):
@@ -59,7 +85,7 @@ def main(args=None):
                     rv = pkt.rotationVector        # quaternion from BNO086
 
                     node.publish_quaternion(rv.i, rv.j, rv.k, rv.real)
-                    
+
     except (KeyboardInterrupt, ExternalShutdownException):
         node.get_logger().info('Interrupt caught, allowing rclpy to shutdown.')
     finally:
