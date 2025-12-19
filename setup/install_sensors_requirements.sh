5059af8057212940a469aae6fb5ab8da1a4c47e3
Sebastian Fryklund <sfd21003@student.mdu.se>
2025-12-19 09:38:01 +0100

    changed order so lidar SDK can be installed

--- setup/install_sensors_requirements.sh
+++ setup/install_sensors_requirements.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+### ROS Workspace ###
+
+# Repository with ros_ws folder
+cd ~
+git clone https://github.com/MDU-C2/SLaRC-HeavySeeker-HT25.git slarc
+cd ~/slarc
+git submodule update --init --recursive
+
 ### non ROS sensor requirements ###
 
 # Camera
@@ -11,16 +19,7 @@
 sudo netplan set ethernets.enx1a3202991545.dhcp4=true
 sudo netplan apply
 
-
-### ROS Workspace ###
-
-# Repository with ros_ws folder
-cd ~
-git clone https://github.com/MDU-C2/SLaRC-HeavySeeker-HT25.git slarc
-cd ~/slarc
-git submodule update --init --recursive
-
-# Install all ros dependencies
+### ROS dependencies ###
 cd ~/slarc/ros_ws
 rosdep install --from-path src --ignore-src -y -r
 
