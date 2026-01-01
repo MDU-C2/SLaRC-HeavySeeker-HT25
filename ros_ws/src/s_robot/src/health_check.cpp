aaaac58762bea80d345802bae08ce90723fdaa68
Sebastian Fryklund <sfd21003@student.mdu.se>
2026-01-01 19:43:04 +0100

    updated to use twist_mux and check actual hardware states

--- ros_ws/src/s_robot/src/health_check.cpp
+++ ros_ws/src/s_robot/src/health_check.cpp
@@ -6,6 +6,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/bool.hpp>
 #include <sensor_msgs/msg/joy.hpp>
+#include <geometry_msgs/msg/twist_stamped.hpp>
 #include "s_msgs/msg/operation_modes.hpp"
 #include "s_robot/status_info.hpp"
 
@@ -15,9 +16,7 @@
  * an heartbeat functionality. The heartbeat is a message sent every 500ms that includes information about 
  * the allowed operation modes.
  * 
- * Subscribers:
- *  - /emergency_stop - std_msgs/msg/Bool - state of emergency stop, pressed=True
- * 
+ * Subscribers: 
  *  - /battery_info - s_msgs/msg/BatterInfo - info about current battery status
  *  - /motor_info - s_msgs/msg/MotorInfo - info about current motor status
  * 
@@ -30,42 +29,37 @@ class HealthCheckNode : public rclcpp::Node {
 
     public:
     HealthCheckNode()
-     : Node("health_check"), h_emergency_stop(true) {
-
-        rclcpp::QoS qos_profile(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_sensor_data));
-        qos_profile.reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
+     : Node("health_check") {
 
         // set initial allowed modes before sensor data can be read
         allowed_modes.local_control = false;
         allowed_modes.remote_control = false;
         allowed_modes.autonomy_drive = false;
 
-        sub_emergency_stop = this->create_subscription<std_msgs::msg::Bool>("/emergency_stop", qos_profile, std::bind(&HealthCheckNode::callback_emergency_stop, this, std::placeholders::_1));
-
         pub_allowed_operation_modes = this->create_publisher<s_msgs::msg::OperationModes>("/allowed_operation_modes", 10);
         
         using namespace std::chrono_literals;
         timer_health_update = this->create_timer(500ms, std::bind(&HealthCheckNode::callback_timer_health_update, this));
 
-        RCLCPP_INFO(this->get_logger(), "Health Checks Is Running");
+        
 
         // declare ros params
-        this->declare_parameter<std::string>("platform.motor", "/motors_ok");
+        this->declare_parameter<std::string>("platform.motor", "/motor_ok");
         this->declare_parameter<std::string>("platform.battery", "/battery_ok");
         this->declare_parameter<std::vector<std::string>>("manual_controllers.topics", std::vector<std::string>{});
+
         this->declare_parameter<std::string>("requirements.telop.fpv_camera", "");
         this->declare_parameter<std::string>("requirements.autonomy.lidar", "");
         this->declare_parameter<std::string>("requirements.autonomy.camera1", "");
         this->declare_parameter<std::string>("requirements.autonomy.camera2", "");
 
-        telop_req_topics[0] = this->get_parameter("requirements.telop.fpv_camera").as_string();
 
+        telop_req_topics[0] = this->get_parameter("requirements.telop.fpv_camera").as_string();
         autonomy_req_topics[0] = this->get_parameter("requirements.autonomy.lidar").as_string();
         autonomy_req_topics[1] = this->get_parameter("requirements.autonomy.camera1").as_string();
         autonomy_req_topics[2] = this->get_parameter("requirements.autonomy.camera2").as_string();
 
-        
-        // Platform status subscribers
+        // Platform status subscribers        
         sub_platform[0] = this->create_subscription<std_msgs::msg::Bool>(this->get_parameter("platform.motor").as_string(), 10, std::bind(&HealthCheckNode::callback_sub_platform_motor, this, std::placeholders::_1));
         sub_platform[1] = this->create_subscription<std_msgs::msg::Bool>(this->get_parameter("platform.battery").as_string(), 10, std::bind(&HealthCheckNode::callback_sub_platform_battery, this, std::placeholders::_1));
 
@@ -78,7 +72,8 @@ class HealthCheckNode : public rclcpp::Node {
         // sensor subscribtions
 
         // publishers
-        pub_saftey_stop = this->create_publisher<std_msgs::msg::Bool>("/saftey_stop", 10);
+        pub_saftey_stop = this->create_publisher<std_msgs::msg::Bool>("/safety_stop", 10);
+        pub_zero_vel = this->create_publisher<geometry_msgs::msg::TwistStamped>("/emergency/cmd_vel", 10);
     }
 
     private:
@@ -100,9 +95,10 @@ class HealthCheckNode : public rclcpp::Node {
 
     Status<sensor_msgs::msg::Joy> status_controller;
 
-
+    std_msgs::msg::Bool lock_telop_control, lock_local_control, lock_auto_control;
     std_msgs::msg::Bool saftey_stop;
-    rclcpp::Time current_time;
+    geometry_msgs::msg::TwistStamped zero_vel;
+    rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr pub_zero_vel;
 
     void callback_sub_platform_motor(std_msgs::msg::Bool motorReady){
         this->status_motor.last_msg = motorReady;
@@ -126,34 +122,54 @@ class HealthCheckNode : public rclcpp::Node {
         }
     }
 
+    void callback_sub_manual_controllers(sensor_msgs::msg::Joy joy) {
+        
+        // this->status_controller.last_msg = joy;
+
+        // only save time recived, the actual input is not used
+        this->status_controller.time_recived = this->get_clock()->now();
+    }
+
+    void activate_safety_lock() {
+        // send 0 velocity
+        this->zero_vel.header.stamp = this->get_clock()->now();
+        pub_zero_vel->publish(this->zero_vel);
+
+        // activate lock
+        saftey_stop.data = true;
+        pub_saftey_stop->publish(this->saftey_stop);
+    }
+
     void platform_saftey_check() {
 
+        // check that hardware is OK
         for (const Status<std_msgs::msg::Bool> &status : status_platform) { 
             // check platform message age
             if (this->get_clock()->now().nanoseconds() - status.time_recived.nanoseconds() > 600*1000000) {
-                saftey_stop.data = true;
-                pub_saftey_stop->publish(this->saftey_stop);
+                activate_safety_lock();
                 return;
             }
 
             // check platform status
             if (status.last_msg.data == false) {
-                saftey_stop.data = true;
-                pub_saftey_stop->publish(this->saftey_stop);
+                activate_safety_lock();
                 return;
             }
         }
 
-        // all topics are up to date and delivers system ok
+        // check for manual controllers
+        if (this->get_clock()->now().nanoseconds() - this->status_controller.time_recived.nanoseconds() > 600*1000000) {
+            activate_safety_lock();
+            return;
+        }
+
+        // all topics are up to date and delivers system ok, manual controller connected
         saftey_stop.data = false;
         pub_saftey_stop->publish(saftey_stop);
     
     }
 
-    void callback_sub_manual_controllers(sensor_msgs::msg::Joy joy) {
-        this->status_controller.last_msg = joy;
-        this->status_controller.time_recived = this->get_clock()->now();
-    }
+    
 
     // All things below is old ------------------------------------------------------
     private:
@@ -175,35 +191,10 @@ class HealthCheckNode : public rclcpp::Node {
         return;
     }
 
-    // callback for emergency button
-    void callback_emergency_stop(std_msgs::msg::Bool emergency_stop) {
-
-        // do nothing as nothing has changed
-        if (h_emergency_stop == emergency_stop.data)
-            return;
-
-        h_emergency_stop = emergency_stop.data;
-
-        if(h_emergency_stop) {
-            RCLCPP_INFO(this->get_logger(), "Emergency Button Pressed");
-            allowed_modes.local_control = false;
-            allowed_modes.remote_control = false;
-            allowed_modes.autonomy_drive = false;
-        }
-        else {
-            RCLCPP_INFO(this->get_logger(), "Emergency Button Relesed");
-            checkHealth();
-        }
-
-        publishAllowedModes();
-    }
-
     rclcpp::Publisher<s_msgs::msg::OperationModes>::SharedPtr pub_allowed_operation_modes;
     rclcpp::TimerBase::SharedPtr timer_health_update;
 
-    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr sub_emergency_stop;
 
-    bool h_emergency_stop;
     s_msgs::msg::OperationModes allowed_modes;
 
     std::map<std::string, std::vector<std::string>> topics;
@@ -218,12 +209,6 @@ class HealthCheckNode : public rclcpp::Node {
         //}
 
         // check if the registred sensors are delivering data
-
-
-
-        // dont update status if emergency button is pressed
-        if(this->h_emergency_stop)
-            return;
         
         this->allowed_modes.local_control = true;
         this->allowed_modes.remote_control = true;
