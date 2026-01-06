3c5ac960d4eaa412c956af2ac06fc2057663e5a2
Sebastian Fryklund <sfd21003@student.mdu.se>
2026-01-06 22:48:11 +0100

    hardware interface testing

---
+++ ros_ws/src/example_2/hardware/include/ros2_control_demo_example_2/can_bus.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <net/if.h>
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+
+const std::string MOTOR_ADAPTER_ID = "000D000A5656430C20393039";
+const std::string BATTERY_ADAPTER_ID = "8004001C5656430C20393039";
+const std::string BITRATE_str = "1000000";
+
+class CanBus {
+    
+    public:
+    CanBus();
+    CanBus(const std::string &adapter_ID);
+    CanBus(const std::string &interface_Name, bool use_ifacename);
+    ~CanBus();
+
+    void setup_with_ID(const std::string &adapter_ID);
+    void setup_with_Interface(const std::string &interface_Name);
+
+    void send_frame(const struct can_frame &frame);
+    ssize_t recive_frame(struct can_frame &frame);
+
+    private:
+    void get_interface_from_ID(const std::string &adapter_ID);
+    void bring_up_can();
+    void setup_socket();
+    
+    std::string m_interface;
+    
+    int m_socket;
+    struct ifreq m_ifr;
+    struct sockaddr_can m_addr;
+    
+
+};
+
+
+
